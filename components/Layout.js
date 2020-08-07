c5d7350e790621461f38a825fce0d8f68d21c68c
Josh Larson <jplhomer@gmail.com>
2020-08-07 12:42:51 -0500

    Add logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,16 @@
 import Sidebar from "./Sidebar";
 import Link from "flareact/link";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
   return (
     <div className="h-full flex flex-col">
       <nav className="flex items-center h-16 px-6 fixed inset-x-0 top-0 z-20 border-b bg-white">
         <Link href="/">
-          <a>Flareact</a>
+          <a>
+            <span className="sr-only">Flareact</span>
+            <Logo className="fill-current w-32 h-12 block" />
+          </a>
         </Link>
       </nav>
       <div className="flex flex-1 h-full">

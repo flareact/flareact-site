6fc6939bf450082ea21dc8cf144d7956867e8e3b
Josh Larson <jplhomer@gmail.com>
2020-08-23 16:29:22 -0500

    Add meta description to homepage

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Index() {
         <title>
           Flareact - Edge-Rendered React Framework built for Cloudflare Workers
         </title>
+        <meta
+          name="description"
+          content="Flareact is an edge-rendered React framework built for Cloudflare Workers. It features file-based page routing with dynamic page paths and **edge-side data fetching APIs."
+        />
       </Head>
       <div className="prose" dangerouslySetInnerHTML={{ __html: Content }} />
     </Layout>

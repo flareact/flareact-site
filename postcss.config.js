9f5caab173b8338717c6bdfce326e1faa9202528
Josh Larson <jplhomer@gmail.com>
2020-12-08 23:32:47 -0600

    Upgrade to tailwind v2

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("postcss-flexbugs-fixes"),
-    require("postcss-preset-env")({
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      stage: 3,
-      features: {
-        "custom-properties": false,
-      },
-    }),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
